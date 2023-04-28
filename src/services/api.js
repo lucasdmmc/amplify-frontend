9d5b955022e68bee795a5150eeba47a020cef876
Ed Willy Pires <lawiletxd@live.com>
2023-04-28 02:30:47 -0300

    Fixing baseURL for the 100000th time

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "https://https://amplifyfrontendmaster.gatsbyjs.io"
+  baseURL: "https://amplifyfrontendmaster.gatsbyjs.io"
 })
