c104a08e0a36be4820d8554e0acd89490e8d83b9
Ed Willy Pires <lawiletxd@live.com>
2023-04-28 02:00:29 -0300

    fixing base url -.- im so dumb

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "https://https://amplifyfrontendmaster.gatsbyjs.io/:3000"
+  baseURL: "https://https://amplifyfrontendmaster.gatsbyjs.io/"
 })
