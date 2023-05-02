9b07306c6064662c6e7e9e62866036bdaaf6d11d
lucasdmmc <lucas.dmmc@hotmail.com>
2023-05-02 18:02:52 -0300

    minor adjustments

---
+++ src/components/FaceLivenessDetector/index.jsx
@@ -0,0 +1,121 @@
+import React, {useState, useEffect} from 'react';
+import { GlobalStyle } from '../../styles/global';
+import { ThemeProvider, Alert, useTheme } from '@aws-amplify/ui-react';
+import { createSessionId, handleAnalysisComplete } from '../../services/LivenessHelper';
+import { FaceLivenessDetector } from '@aws-amplify/ui-react-liveness';
+
+
+export const FaceLiveness = () => {
+  const [verified, setVerified] = useState(false);
+  const [sessionId, setSessionId] = useState(null);
+  const [referenceImage, setReferenceImage] = useState(null);
+  const [confidence, setConfidence] = useState(null);
+  const { tokens } = useTheme();
+
+  const theme = {
+    name: 'Face Liveness Example Theme',
+    tokens: {
+      colors: {
+        background: {
+          primary: {
+            value: tokens.colors.neutral['90'].value,
+          },
+          secondary: {
+            value: tokens.colors.neutral['100'].value,
+          },
+        },
+        font: {
+          primary: {
+            value: tokens.colors.white.value,
+          },
+        },
+        brand: {
+          primary: {
+            '10': tokens.colors.teal['100'],
+            '80': tokens.colors.teal['40'],
+            '90': tokens.colors.teal['20'],
+            '100': tokens.colors.teal['10'],
+          },
+        },
+      },
+    },
+  };
+
+
+  const callCreateSession = async () => {
+    if(sessionId) {
+      setSessionId(null)
+    }
+    const sessionid = await createSessionId()
+    setSessionId(sessionid)
+  }
+
+  const callHandleAnalysisComplete = async () => {
+    try {
+        const analysisResult = await handleAnalysisComplete(sessionId)
+        if(analysisResult.verified) {
+          setVerified(true)
+          setReferenceImage(analysisResult.referenceImageURL)
+          setConfidence(analysisResult.confidence)
+        } else {
+          alert("User not verified! Please, try again")
+          callCreateSession()
+        }
+    } catch (error){
+        alert("It was not possible to handle the analysis results " + error)
+        callCreateSession()
+    }
+  }
+  
+
+  useEffect(() => {
+    if(!sessionId) {
+      callCreateSession()
+    }
+  },[])
+  return (
+    <>
+    <>
+      <GlobalStyle />
+      {verified && referenceImage ? 
+      <div style={{display: "flex", flexDirection: "column", justifyContent: "center"}}>
+        <h1 style={{margin: "auto", color: "#07eb07"}}>The user is validated as a real person</h1>
+        <img src={referenceImage} alt="" style={{width: '400px', margin: "auto"}}/>
+        <h2 style={{margin: "auto"}}>Aproximated assurance: {Math.round(confidence)}%</h2>
+      </div>
+      : (
+        <ThemeProvider theme={theme}>
+
+        {sessionId ? (
+          <FaceLivenessDetector
+          sessionId={sessionId}
+          region='us-east-1'
+          onAnalysisComplete={callHandleAnalysisComplete}
+          onUserCancel={() => callCreateSession()}
+            components={{
+              PhotosensitiveWarning: () => {
+                return (
+                  <Alert
+                    backgroundColor={"#e7c35e"}
+                    variation='info'
+                    isDismissible={false}
+                    hasIcon={true}
+                    heading="Caution"
+                  >
+                  This check displays colored lights. Use caution if you are
+                  photosensitive.
+                 </Alert>
+                )
+              },
+            }}
+          />
+        ) : (
+          <p>Loading...</p>
+        )}
+      </ThemeProvider>
+      )
+    }
+    </>
+    </>
+  )
+}
