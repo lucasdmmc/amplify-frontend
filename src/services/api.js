f8d552da19d0a6f635e5586fa68aeace49ee79e4
Ed Willy Pires <lawiletxd@live.com>
2023-05-10 17:03:47 -0300

    uploading aws swap changes

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,6 @@ import axios from "axios"
 //https://faceliveness-backend.onrender.com/
 // https://faceliveness-api.onrender.com
 export const api = axios.create({
-  baseURL: "http://localhost:3000"
-  // baseURL: "https://faceliveness-api.onrender.com"
+  //baseURL: "http://localhost:3000"
+  baseURL: "https://faceliveness-api.onrender.com"
 })
