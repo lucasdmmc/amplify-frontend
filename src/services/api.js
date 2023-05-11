8bddafbaf2b9e28d59b4e2e91176904b7c0cd9be
william <william.william.ho@gmail.com>
2023-05-11 10:59:52 -0300

    minot adjustments

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,5 @@ import axios from "axios"
 //https://faceliveness-backend.onrender.com/
 // https://faceliveness-api.onrender.com
 export const api = axios.create({
-  baseURL: "https://faceliveness-api.onrender.com"
+  baseURL: "https://faceliveness-api.herokuapp.com/"
 })
